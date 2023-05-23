f2fc129ab768f44db7325eb2ed100e2d499cc530
Max Martinez <maximartinez@me.com>
2023-05-23 15:56:26 +0100

    CRUD done

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   get "tasks/new", to: "tasks#new"
   get "tasks/:id", to: "tasks#show", as: "task"
   post "tasks", to: "tasks#create", as: "post"
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy", as: "destroy"
 end
